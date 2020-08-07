e778d53a551b56c44be66119d4e18a205fefde77
vsreyes <virgilio.s.reyes@gmail.com>
2020-08-07 22:50:17 +0200

    Added forEach

--- script.js
+++ script.js
@@ -23,5 +23,34 @@ async function getRandomUser() {
         money: Math.floor(Math.random() * 1000000)
     };
 
-    console.log(newUser);
+    addData(newUser);
 }
+
+// Add new obj to data arr
+function addData(obj) {
+    data.push(obj);
+
+    updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+    // Clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+}
+
+// Format number as money
+function formatMoney(number) {
+    return '$ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  }
+
+
+  // Event listeners
+  addUserBtn.addEventListener('click', getRandomUser);
