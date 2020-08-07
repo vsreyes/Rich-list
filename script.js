b2a48f8b7a111a634f228a509f956cc19828a21f
vsreyes <virgilio.s.reyes@gmail.com>
2020-08-07 23:31:51 +0200

    Filter millionaires

--- script.js
+++ script.js
@@ -42,6 +42,13 @@ function sortByRichest() {
     updateDOM();
 }
 
+// Filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
     data.push(obj);
@@ -72,4 +79,5 @@ function formatMoney(number) {
   addUserBtn.addEventListener('click', getRandomUser);
   doubleBtn.addEventListener('click', doubleMoney);
   sortBtn.addEventListener('click', sortByRichest);
+  showMillionairesBtn.addEventListener('click', showMillionaires);
 
