1cea6b36e0b766ccc3e7d99ea7ef36016350d234
vsreyes <virgilio.s.reyes@gmail.com>
2020-08-07 23:18:10 +0200

    Sort by riches

--- script.js
+++ script.js
@@ -35,6 +35,13 @@ function doubleMoney() {
     updateDOM();
 }
 
+// Sort users by richest
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
     data.push(obj);
@@ -64,4 +71,5 @@ function formatMoney(number) {
   // Event listeners
   addUserBtn.addEventListener('click', getRandomUser);
   doubleBtn.addEventListener('click', doubleMoney);
+  sortBtn.addEventListener('click', sortByRichest);
 
